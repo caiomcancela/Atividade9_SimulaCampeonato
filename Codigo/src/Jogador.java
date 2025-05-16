2b7e11784bcf7ee71c36e3b2b1a1b2a47c97d184
Caio Moreira Cancela <caiomcancela.18@gmail.com>
2025-05-16 19:55:24 -0300

    Estrutura inicial classe Jogador

--- Codigo/src/Jogador.java
+++ Codigo/src/Jogador.java
@@ -1,2 +1,41 @@
 public class Jogador {
+    private String nome;
+    private Integer Idade;
+    private String posicao;
+    private TimeFutebol timeAtual;
+
+    public Jogador(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Integer getIdade() {
+        return Idade;
+    }
+
+    public void setIdade(Integer idade) {
+        Idade = idade;
+    }
+
+    public String getPosicao() {
+        return posicao;
+    }
+
+    public void setPosicao(String posicao) {
+        this.posicao = posicao;
+    }
+
+    public void adicionarTime(TimeFutebol time){
+        timeAtual = time;
+    }
+    public  timeQueJogaAtualmente(){
+        return "O jogador " + this.nome + "pertence ao time " + timeAtual.getNome() +"\n";
+    }
+
+    public String jogar(){
+        return "O jogador " + this.nome + "esta jogando" + "\n";
+    }
 }
