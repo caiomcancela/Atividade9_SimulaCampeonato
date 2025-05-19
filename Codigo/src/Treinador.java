3186e19289b0eea47c6e3e9c714d502cbce16059
Juan Pablo <juanpablonferraz@gmail.com>
2025-05-19 19:53:39 -0300

    Implementação da classe Treinador.

--- Codigo/src/Treinador.java
+++ Codigo/src/Treinador.java
@@ -1,2 +1,54 @@
+import java.util.ArrayList;
+
 public class Treinador {
+    private String nome;
+    private Integer idade;
+    private String EstiloDeJogo;
+    private TimeEsportivo Time;
+
+    public Treinador() {
+        this.nome = "";
+        this.idade = 0;
+        this.EstiloDeJogo = "";
+
+    }
+
+    public void addTime(TimeEsportivo time){
+        Time = time;
+    }
+
+    public String treinarTime(TimeEsportivo time){
+        return "Nome do time: " + time.setNome() +
+                "Escalacao:   " + time.listarJogadores() +
+                "Estilo de jogo:" + EstiloDeJogo;
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+
+    public String getEstiloDeJogo() {
+        return EstiloDeJogo;
+    }
+
+    public void setEstiloDeJogo(String estiloDeJogo) {
+        EstiloDeJogo = estiloDeJogo;
+    }
+
+
 }
+
+
