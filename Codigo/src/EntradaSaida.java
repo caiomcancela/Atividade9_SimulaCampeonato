0441db112bf6134e5f6f8a0241ea6f830f56fae7
Joao Pedro Furiati <joaopedrofsutana@gmail.com>
2025-05-19 09:55:18 -0300

    adição dos códigos, de base, das classes Main e EntradaSaida

--- Codigo/src/EntradaSaida.java
+++ Codigo/src/EntradaSaida.java
@@ -1,2 +1,50 @@
+import javax.swing.*;
+
 public class EntradaSaida {
+
+    public static void menu() {
+        String[] opcoes = {"1 - Cadastrar Times", "2 - Simular Jogos", "3 - Sair"};
+        int escolha = JOptionPane.showOptionDialog(
+                null, "Selecione a opção desejada:", "Menu Principal",
+                JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, opcoes, opcoes[0]);
+
+        switch (escolha) {
+            case 0:
+                String nomeTime = lerString("Digite o nome do time:");
+                JOptionPane.showMessageDialog(null, "Time cadastrado: " + nomeTime);
+                break;
+
+            case 1:
+                JOptionPane.showMessageDialog(null, "");
+                break;
+
+            case 2:
+                JOptionPane.showMessageDialog(null, "Saindo do programa...");
+                System.exit(0);
+                break;
+
+            default:
+                JOptionPane.showMessageDialog(null, "Nenhuma opção válida foi selecionada.");
+        }
+    }
+
+    public static int lerInteiro(String mensagem) {
+        String valor = JOptionPane.showInputDialog(mensagem);
+        return Integer.parseInt(valor);
+    }
+
+    public static String lerString(String mensagem) {
+        String nome = JOptionPane.showInputDialog(mensagem);
+        return nome;
+    }
+
+    public static double lerDouble(String mensagem) {
+        String valor2 = JOptionPane.showInputDialog(mensagem);
+        return Double.parseDouble(valor2);
+    }
+
+    public static String exibir(String mensagem) {
+        String msg = JOptionPane.showInputDialog(mensagem);
+        return msg;
+    }
 }
--- Codigo/src/Main.java
+++ Codigo/src/Main.java
@@ -1,7 +1,18 @@
-import java.sql.SQLOutput;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Teste");
+
+        EntradaSaida.menu();
+
+        int num = EntradaSaida.lerInteiro("Digite um numero");
+        System.out.println(num);
+
+        String nome =  EntradaSaida.lerString("Digite o nome do time:");
+        System.out.println(nome);
+
+        double valor = EntradaSaida.lerDouble("Digite um valor");
+        System.out.println(valor);
+
+        String msg = EntradaSaida.exibir("Digite uma mensagem");
+        System.out.println(msg);
     }
 }
